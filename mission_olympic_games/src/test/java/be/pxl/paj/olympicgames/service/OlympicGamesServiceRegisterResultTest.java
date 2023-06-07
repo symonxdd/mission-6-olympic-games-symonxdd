992317a2d917eb89cc515032be2a6a8b4e46dc71
Symon <symon.bla@gmail.com>
2023-06-07 02:30:55 +0200

    Update OlympicGamesServiceRegisterResultTest.java

--- mission_olympic_games/src/test/java/be/pxl/paj/olympicgames/service/OlympicGamesServiceRegisterResultTest.java
+++ mission_olympic_games/src/test/java/be/pxl/paj/olympicgames/service/OlympicGamesServiceRegisterResultTest.java
@@ -1,7 +1,7 @@
 package be.pxl.paj.olympicgames.service;
 
-import be.pxl.paj.olympicgames.domain.Athlete;
-import be.pxl.paj.olympicgames.domain.Race;
+import be.pxl.paj.olympicgames.api.data.RegisterScoreCommand;
+import be.pxl.paj.olympicgames.domain.*;
 import be.pxl.paj.olympicgames.exception.BusinessException;
 import be.pxl.paj.olympicgames.exception.NotFoundException;
 import be.pxl.paj.olympicgames.repository.AthleteRepository;
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalTime;
 import java.util.Optional;
 
 import static be.pxl.paj.olympicgames.builder.AthleteBuilder.anAthlete;
@@ -65,13 +66,31 @@ public class OlympicGamesServiceRegisterResultTest {
     }
 
     @Test
-    void setScoreTimeWhenTimeIsProvided() {
-        // TODO: check nog eventueel na 😀
+    void setScoreTimeWhenTimeIsProvided() { // ♾️ this test is pretty nice
+        Race race = aRace().withDiscipline(Discipline.HORDES_400M).build();
+        when(raceRepository.findById(RACE_ID)).thenReturn(Optional.of(race));
+        Athlete athlete = anAthlete().withDiscipline(Discipline.HORDES_400M).build();
+        when(athleteRepository.findById(ATHLETE_ID)).thenReturn(Optional.of(athlete));
 
-        when(raceRepository.findById(any(Long.class))).thenReturn(Optional.of(aRace().build()));
-        when(athleteRepository.findById(any(Long.class))).thenReturn(Optional.of(anAthlete().build()));
-        BusinessException ex =
-                assertThrows(BusinessException.class, () -> olympicGamesService.registerResult(RACE_ID, ATHLETE_ID, aRegisterScoreCommand().build()));
-        assertEquals("Athlete [" + ATHLETE_ID + "] did not enroll in race.", ex.getMessage());
+        // zorg dat de athlete deelneemt aan de race
+        race.addParticipant(athlete);
+        RegisterScoreCommand registerScoreCommand = aRegisterScoreCommand().withStatus(ScoreStatus.QUALIFIED).withTime(LocalTime.of(0, 5)).build();
+        olympicGamesService.registerResult(RACE_ID, ATHLETE_ID, registerScoreCommand);
+        Score score = race.getScore(athlete).get();
+
+        // verify the score is registered
+        assertEquals(ScoreStatus.QUALIFIED, score.getScoreStatus());
+        assertEquals(LocalTime.of(0, 5), score.getTime());
     }
+
+    // @Test
+    // void setScoreTimeWhenTimeIsProvided() {
+    //     // TODO: check nog eventueel na 😀
+    //
+    //     when(raceRepository.findById(any(Long.class))).thenReturn(Optional.of(aRace().build()));
+    //     when(athleteRepository.findById(any(Long.class))).thenReturn(Optional.of(anAthlete().build()));
+    //     BusinessException ex =
+    //             assertThrows(BusinessException.class, () -> olympicGamesService.registerResult(RACE_ID, ATHLETE_ID, aRegisterScoreCommand().build()));
+    //     assertEquals("Athlete [" + ATHLETE_ID + "] did not enroll in race.", ex.getMessage());
+    // }
 }
